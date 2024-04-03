089e3ccd72d3ea978026c1e5ede79a90bdae97b9
Satabdi <satabdiraniweb@gmail.com>
2024-04-03 23:49:56 +0600

    First functionality working

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,22 @@
+// function play(){
+//     // hide the home screen and 
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log(homeSection.classList);
+//     // show the play ground
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+//     // playGround.classList.add('block');
+// }
+
+function continueGame(){
+    // Generate a random Alphabet
+    
+}
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
+}
+
